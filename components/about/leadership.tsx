124e0a0a716170379111ba2177e45ae2972740cb
gidoo5331 <gideonbempong@gmail.com>
2026-09-13 19:24:22 +0000

    add staff members

--- components/about/leadership.tsx
+++ components/about/leadership.tsx
@@ -10,9 +10,11 @@ export function Leadership() {
   return (
     <section className="mx-auto max-w-6xl px-4 py-16 sm:px-6 lg:px-8">
       <SectionTitle eyebrow="Leadership" title="Meet Our Leadership" align="center" className="mx-auto" />
-      <div className="mt-10 grid grid-cols-2 gap-8 sm:grid-cols-3 lg:grid-cols-6">
+      <div className="mt-10 flex flex-wrap justify-center gap-12 sm:gap-20">
         {leadership.map((staff) => (
-          <StaffCard key={staff.id} staff={staff} />
+          <div key={staff.id} className="w-36 sm:w-44">
+            <StaffCard staff={staff} />
+          </div>
         ))}
       </div>
       <div className="mt-8 text-center">
